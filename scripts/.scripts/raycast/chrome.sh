35fe71f7576c358dcc6deccf9b1a9f12a3e7bc06
gogongxt <gogongxt@163.com>
2025-06-05 02:40:57 +0800

    add raycast scripts

---
+++ scripts/.scripts/raycast/chrome.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title Google Chrome Create New Window
+# @raycast.mode compact
+
+# Optional parameters:
+# @raycast.icon /Applications/Google Chrome.app/Contents/Resources/app.icns
+
+
+open -na Google\ Chrome
