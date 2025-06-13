af5e7bba01dd45904819c696912dc855f5fe3063
gogongxt <gogongxt@163.com>
2025-06-14 01:29:52 +0800

    add new kitty script for raycast

---
+++ scripts/.scripts/raycast/kitty.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title Kitty Create New Window
+# @raycast.mode compact
+
+# Optional parameters:
+# @raycast.icon /Applications/kitty.app/Contents/Resources/kitty.icns
+
+
+open -na Kitty
+
