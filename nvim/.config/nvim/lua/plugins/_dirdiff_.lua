a577b76a46037125cd2e9864f43804e330b7f2ac
gogongxt <gogongxt@163.com>
2025-07-14 02:34:39 +0800

    update nvim plugin dirdiff

---
+++ nvim/.config/nvim/lua/plugins/_dirdiff_.lua
@@ -0,0 +1,12 @@
+-- if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
+
+-- use: :ZFDirDiff path1 path2
+-- if has space use: :ZFDirDiff path\ 1 path\ 2
+return {
+  {
+    "ZSaberLv0/ZFVimDirDiff",
+    dependencies = {
+      "ZSaberLv0/ZFVimJob",
+    },
+  },
+}
