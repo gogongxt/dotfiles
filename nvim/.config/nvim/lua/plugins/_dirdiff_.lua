189adbb31fd17e0e505c3b049c4d5da772202ba9
gogongxt <gogongxt@163.com>
2025-07-14 17:33:30 +0800

    update plugin dirdiff

--- nvim/.config/nvim/lua/plugins/_dirdiff_.lua
+++ nvim/.config/nvim/lua/plugins/_dirdiff_.lua
@@ -7,6 +7,39 @@ return {
     "ZSaberLv0/ZFVimDirDiff",
     dependencies = {
       "ZSaberLv0/ZFVimJob",
+      "ZSaberLv0/ZFVimIgnore",
     },
+    cmd = "ZFDirDiff", -- Recommended: load on command
+    config = function()
+      -- ZFVimDirDiff uses global variables for configuration.
+      -- It's recommended to set them before the plugin is loaded,
+      -- which can be done in the `init` function.
+    end,
+    init = function()
+      -- Example: Ignore common development directories and files
+      -- This is a list of glob patterns.
+      vim.g.ZFIgnore_userPat_ZFDirDiff = {
+        ".git",
+        ".svn",
+        "node_modules",
+        "dist",
+        "build",
+        "*.pyc",
+        "*.swp",
+        "*.swo",
+        "__pycache__",
+      }
+
+      vim.g.ZFDirDiffKeymap_update = { "i" }
+      vim.g.ZFDirDiffKeymap_open = { "<cr>", "o", "l", "h" }
+      vim.g.ZFDirDiffKeymap_foldClose = { "h" }
+
+      -- Show hidden files (default is 1)
+      vim.g.ZFDirDiff_showHidden = 1
+
+      -- Compare by file content (default is 'content')
+      -- Other options: 'size', 'timestamp'
+      vim.g.ZFDirDiff_compareOption = "content"
+    end,
   },
 }
