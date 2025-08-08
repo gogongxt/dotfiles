1cc7e1a3a33a62a30dcc1f43a21dbfd39f61c0db
gogongxt <gogongxt@163.com>
2025-08-08 16:12:25 +0800

    update ssh config

--- scripts/.scripts/ssh/myssh.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-CUR_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-
-if [ $# -eq 0 ]; then
-  python3 "$CUR_DIR/myssh_password.py"
-else
-  python3 "$CUR_DIR/myssh_plain_password.py" "$@"
-fi
