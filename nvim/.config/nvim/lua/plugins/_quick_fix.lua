2ac4f4bf7336d76355825e4929f132371384a979
gogongxt <gogongxt@163.com>
2025-08-15 11:28:44 +0800

    update nvim better quick fix

--- nvim/.config/nvim/lua/plugins/_quick_fix.lua
+++ nvim/.config/nvim/lua/plugins/_quick_fix.lua
@@ -1,5 +1,12 @@
 -- if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
 
+vim.cmd [[
+    hi BqfPreviewBorder guifg=#dd7878 ctermfg=71
+    hi BqfPreviewTitle guifg=#ea76cb ctermfg=71
+    hi BqfPreviewThumb guibg=#dd7878 ctermbg=71
+    hi link BqfPreviewRange Search
+]]
+
 return {
   {
     -- better quickfix
@@ -7,8 +14,12 @@ return {
     "kevinhwang91/nvim-bqf",
     ft = "qf",
     config = function()
-      -- require("bqf").setup({
-      -- })
+      require("bqf").setup {
+        func_map = {
+          ptogglemode = "<a-s-p>",
+          ptoggleauto = "<a-p>",
+        },
+      }
     end,
   },
 }
