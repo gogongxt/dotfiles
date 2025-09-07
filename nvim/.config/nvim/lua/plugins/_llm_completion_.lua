febc4be9e2e0894cb5b63f9ba0f94556e2c3bffb
gogongxt <gogongxt@163.com>
2025-09-07 22:27:28 +0800

    update

--- nvim/.config/nvim/lua/plugins/_llm_completion_.lua
+++ nvim/.config/nvim/lua/plugins/_llm_completion_.lua
@@ -1,5 +1,13 @@
 -- if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
 
+-- 从环境变量读取配置
+local api_key = os.getenv "NVIM_LLM_API_KEY" and "NVIM_LLM_API_KEY" or "TERM"
+local end_point = os.getenv "NVIM_LLM_END_POINT" or "http://localhost:11434/v1/completions"
+local model = os.getenv "NVIM_LLM_MODEL" or "qwen2.5-coder:1.5b"
+local llm_enable = os.getenv "NVIM_LLM_ENABLE" or false
+
+if not llm_enable or llm_enable == "false" or llm_enable == "0" then return {} end
+
 return {
   {
     "milanglacier/minuet-ai.nvim",
@@ -22,6 +30,7 @@ return {
             next = "<A-n>",
             dismiss = "<A-e>",
           },
+          show_on_completion_menu = true,
         },
         provider = "openai_fim_compatible",
         n_completions = 3, -- recommend for local model for resource saving
@@ -30,12 +39,10 @@ return {
           openai_fim_compatible = {
             -- For Windows users, TERM may not be present in environment variables.
             -- Consider using APPDATA instead.
-            api_key = "API_KEY",
+            api_key = api_key,
             name = "Ollama",
-            end_point = "http://$TODO/v1/completions",
-            -- model = "qwen2.5-coder:1.5b",
-            -- model = "qwen3:30b",
-            -- model = "qwen3-coder:30b",
+            end_point = end_point,
+            model = model,
             optional = {
               max_tokens = 56,
               top_p = 0.9,
@@ -44,5 +51,31 @@ return {
         },
       }
     end,
+  },
+  {
+    "saghen/blink.cmp",
+    opts = {
+      sources = {
+        -- Enable minuet for autocomplete
+        default = { "lsp", "path", "buffer", "snippets", "minuet" },
+        -- For manual completion only, remove 'minuet' from default
+        providers = {
+          minuet = {
+            name = "minuet",
+            module = "minuet.blink",
+            async = true,
+            -- Should match minuet.config.request_timeout * 1000,
+            -- since minuet.config.request_timeout is in seconds
+            timeout_ms = 3000,
+            score_offset = 50, -- Gives minuet higher priority among suggestions
+          },
+        },
+      },
+      -- Recommended to avoid unnecessary request
+      completion = {
+        trigger = { prefetch_on_insert = true },
+        ghost_text = { enabled = false },
+      },
+    },
   },
 }
