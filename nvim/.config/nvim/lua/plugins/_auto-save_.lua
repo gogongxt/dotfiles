be611ee99ae8db860b61f4ba18e6e25b13b13557
gogongxt <gogongxt@163.com>
2025-09-26 10:50:34 +0800

    update autosave for claudecode

--- nvim/.config/nvim/lua/plugins/_auto-save_.lua
+++ nvim/.config/nvim/lua/plugins/_auto-save_.lua
@@ -18,6 +18,29 @@ return {
       -- your config goes here
       -- or just leave it empty :)
       debounce_delay = debounce_delay, -- delay after which a pending save is executed
+      -- solve autosave bug: https://github.com/coder/claudecode.nvim#auto-save-plugin-issues
+      condition = function(buf)
+        -- Exclude claudecode diff buffers by buffer name patterns
+        local bufname = vim.api.nvim_buf_get_name(buf)
+        if bufname:match "%(proposed%)" or bufname:match "%(NEW FILE %- proposed%)" or bufname:match "%(New%)" then
+          return false
+        end
+
+        -- Exclude by buffer variables (claudecode sets these)
+        if
+          vim.b[buf].claudecode_diff_tab_name
+          or vim.b[buf].claudecode_diff_new_win
+          or vim.b[buf].claudecode_diff_target_win
+        then
+          return false
+        end
+
+        -- Exclude by buffer type (claudecode diff buffers use "acwrite")
+        local buftype = vim.fn.getbufvar(buf, "&buftype")
+        if buftype == "acwrite" then return false end
+
+        return true -- Safe to auto-save
+      end,
     },
   },
   -- {
