ce9b09987ea3c2979322a4d74aeb1b941029d88c
gogongxt <gogongxt@163.com>
2026-02-03 14:43:15 +0800

    [zsh] update proxy

--- zsh/.sh_help/functions/proxy.sh
+++ zsh/.sh_help/functions/proxy.sh
@@ -4,12 +4,14 @@ hash proxychains4 2>/dev/null && { alias pro='proxychains4'; }
 hash proxychains 2>/dev/null && { alias pro='proxychains'; }
 #🔼🔼🔼
 
+: ${PROXY_DEFAULT:=127.0.0.1:7890}
+
 #🔽🔽🔽
 # set proxy
 # reference https://wiki.archlinux.org/title/Proxy_server
 function proxy_on() {
-    local proxy="127.0.0.1:7890"
-    if (( $# > 0 )); then
+    local proxy="${PROXY:-$PROXY_DEFAULT}"
+    if (($# > 0)); then
         # get "ip:port" format
         valid=$(echo $@ | sed -E 's/([0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}):([0-9]+)/\1:\2/')
         if [[ $valid != $@ ]]; then
@@ -19,29 +21,28 @@ function proxy_on() {
         proxy=$1
     fi
     export http_proxy=$proxy \
-           https_proxy=$proxy \
-           HTTP_PROXY=$proxy \
-           HTTPS_PROXY=$proxy \
-           ftp_proxy=$proxy \
-           FTP_PROXY=$proxy \
-           rsync_proxy=$proxy \
-           RSYNC_PROXY=$proxy
+        https_proxy=$proxy \
+        HTTP_PROXY=$proxy \
+        HTTPS_PROXY=$proxy \
+        ftp_proxy=$proxy \
+        FTP_PROXY=$proxy \
+        rsync_proxy=$proxy \
+        RSYNC_PROXY=$proxy
     echo -e "Proxy environment variable seted."
     echo "Note that sudo will not use proxy"
     echo "proxy=$proxy"
 }
-function proxy_off(){
+function proxy_off() {
     unset http_proxy https_proxy ftp_proxy rsync_proxy \
-          HTTP_PROXY HTTPS_PROXY FTP_PROXY RSYNC_PROXY
+        HTTP_PROXY HTTPS_PROXY FTP_PROXY RSYNC_PROXY
     echo -e "Proxy environment variable removed."
 }
-function proxy_status(){
-    if [ -z ${http_proxy+x} ] && [ -z ${https_proxy+x} ] ;
-    then
-    	echo -e "No Proxy environment."
+function proxy_status() {
+    if [ -z ${http_proxy+x} ] && [ -z ${https_proxy+x} ]; then
+        echo -e "No Proxy environment."
     else
-    	echo -e "Have Proxy environment."
-    fi 
+        echo -e "Have Proxy environment."
+    fi
     echo -e "http_proxy: ${http_proxy}"
     echo -e "HTTP_PROXY: ${HTTP_PROXY}"
     echo -e "https_proxy: ${https_proxy}"
@@ -93,11 +94,13 @@ alias pt="proxy toggle"
 #🔼🔼🔼
 
 # pip install package use http proxy rather than other source
-# export PIP_PROXY="-i https://pypi.org/simple/ --proxy http://127.0.0.1:7890"
-# alias pip_proxy='pip --proxy http://127.0.0.1:7890 -i https://pypi.org/simple/'
-alias pip_proxy_7890="http_proxy=127.0.0.1:7890 https_proxy=127.0.0.1:7890 PIP_INDEX_URL=https://pypi.org/simple pip"
-alias pip_proxy_10099="http_proxy=10.77.22.11:10099 https_proxy=10.77.22.11:10099 PIP_INDEX_URL=https://pypi.org/simple pip"
-alias pip_proxy=pip_proxy_10099
+pip_proxy() {
+    local proxy="${PROXY:-$PROXY_DEFAULT}"
+    http_proxy="$proxy" https_proxy="$proxy" PIP_INDEX_URL="https://pypi.org/simple" pip "$@"
+}
+pip_proxy_10099() {
+    PROXY=10.77.22.11:10099 pip_proxy "$@"
+}
 alias pip_tsinghua="http_proxy= https_proxy= PIP_INDEX_URL=https://pypi.tuna.tsinghua.edu.cn/simple pip"
 alias pip_aliyun="http_proxy= https_proxy= PIP_INDEX_URL=https://mirrors.aliyun.com/pypi/simple pip"
 alias pip_tencent="http_proxy= https_proxy= PIP_INDEX_URL=https://mirrors.cloud.tencent.com/pypi/simple pip"
