5e82ce40d751f25bf2c0a520265763b616bd3cdc
gogongxt <gogongxt@163.com>
2026-03-18 02:01:51 +0800

    [nvim] fix luasnip lazyload to immediately load

--- nvim/.config/nvim/lua/plugins/_snippets_.lua
+++ nvim/.config/nvim/lua/plugins/_snippets_.lua
@@ -4,13 +4,17 @@ return {
   "L3MON4D3/LuaSnip",
   config = function(plugin, opts)
     -- include the default astronvim config that calls the setup call
-    require "astronvim.plugins.configs.luasnip"(plugin, opts)
+    require "astronvim.plugins.configs.luasnip" (plugin, opts)
     -- load snippets paths
-    require("luasnip.loaders.from_vscode").lazy_load {
+    require("luasnip.loaders.from_vscode").load {
       paths = { vim.fn.stdpath "config" .. "/snippets" },
     }
     -- load Lua snippets
     require("luasnip.loaders.from_lua").load {
+      paths = { vim.fn.stdpath "config" .. "/snippets" },
+    }
+    -- load snipmate snippets
+    require("luasnip.loaders.from_snipmate").load {
       paths = { vim.fn.stdpath "config" .. "/snippets" },
     }
   end,
