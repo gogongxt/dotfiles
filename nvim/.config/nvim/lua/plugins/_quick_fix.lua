6a25e5fa9d0449cfd934c5f559238b8d9bd208fe
gogongxt <gogongxt@163.com>
2026-04-13 11:23:48 +0800

    [nvim] bqf add cycle detect layout

--- nvim/.config/nvim/lua/plugins/_quick_fix.lua
+++ nvim/.config/nvim/lua/plugins/_quick_fix.lua
@@ -1,30 +1,31 @@
 -- if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
 
-vim.cmd [[
-    hi BqfPreviewBorder guifg=#dd7878 ctermfg=71
-    hi BqfPreviewTitle guifg=#ea76cb ctermfg=71
-    hi BqfPreviewThumb guibg=#dd7878 ctermbg=71
-    hi link BqfPreviewRange Search
-]]
-
--- 三状态循环：hidden -> normal -> full -> hidden
-local _preview_state = {} -- per qf window state
+-- 检测当前预览状态
+local function get_preview_state(qwinid)
+  local pvs = require "bqf.preview.session"
+  local ps = pvs:get(qwinid)
+  if not ps or not ps:validate() then
+    return "hidden"
+  elseif ps.full then
+    return "full"
+  else
+    return "normal"
+  end
+end
 
 local function cycle_preview()
   local handler = require "bqf.preview.handler"
   local qwinid = vim.api.nvim_get_current_win()
-
-  -- 获取当前状态
-  local state = _preview_state[qwinid] or "hidden"
+  -- 检测当前状态
+  local current_state = get_preview_state(qwinid)
   local next_state
-
-  if state == "hidden" then
+  if current_state == "hidden" then
     next_state = "normal"
     handler.open(qwinid, nil, true)
     -- 确保不是全屏模式
-    local ps = package.loaded["bqf.preview.session"] and require("bqf.preview.session"):get(qwinid)
+    local ps = require("bqf.preview.session"):get(qwinid)
     if ps and ps.full then handler.toggleMode(qwinid) end
-  elseif state == "normal" then
+  elseif current_state == "normal" then
     next_state = "full"
     handler.open(qwinid, nil, true)
     handler.toggleMode(qwinid)
@@ -32,8 +33,6 @@ local function cycle_preview()
     next_state = "hidden"
     handler.close(qwinid)
   end
-
-  _preview_state[qwinid] = next_state
   vim.notify("Preview: " .. next_state, vim.log.levels.INFO)
 end
 
@@ -52,6 +51,18 @@ return {
     "kevinhwang91/nvim-bqf",
     ft = "qf",
     config = function()
+      -- 高亮配置
+      local function set_hl()
+        vim.cmd [[
+          hi BqfPreviewBorder guifg=#dd7878 ctermfg=71
+          hi BqfPreviewTitle guifg=#ea76cb ctermfg=71
+          hi BqfPreviewThumb guibg=#dd7878 ctermbg=71
+          hi link BqfPreviewRange Search
+        ]]
+      end
+      set_hl()
+      vim.api.nvim_create_autocmd("ColorScheme", { callback = set_hl })
+
       require("bqf").setup {
         func_map = {
           ptogglemode = "", -- 禁用默认快捷键
