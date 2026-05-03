e2a00c6e948a93ee3885272c48fafd62531eaf8e
gogongxt <gogongxt@163.com>
2026-05-03 22:40:29 +0800

    [nvim] update flash and remove hop

--- nvim/.config/nvim/lua/plugins/_flash_.lua
+++ nvim/.config/nvim/lua/plugins/_flash_.lua
@@ -4,70 +4,37 @@ return {
   {
     "folke/flash.nvim",
     event = "VeryLazy",
-    opts = {},
-    config = function()
-      require("flash").setup {
-        modes = {
-          search = {
-            enabled = false,
-          },
-          char = {
-            enabled = false,
-          },
-        },
-      }
-    end,
-    keys = {
-      { "<Leader>s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
+    opts = {
+      modes = {
+        search = { enabled = false },
+        char = { enabled = false },
+      },
     },
-  },
-  {
-    "phaazon/hop.nvim",
-    branch = "v2", -- optional but strongly recommended
-    event = "VeryLazy",
-    config = function()
-      -- you can configure Hop the way you like here; see :h hop-config
-      require("hop").setup { keys = "etovxqpdygfblzhckisuran" }
-    end,
-    init = function()
-      local hop = require "hop"
-      local directions = require("hop.hint").HintDirection
-      vim.keymap.set(
-        "",
-        "f",
-        function() hop.hint_char1 { direction = directions.AFTER_CURSOR, current_line_only = true } end,
-        { remap = true, silent = true }
-      )
-      vim.keymap.set(
-        "",
-        "F",
-        function() hop.hint_char1 { direction = directions.BEFORE_CURSOR, current_line_only = true } end,
-        { remap = true, silent = true }
-      )
-      vim.keymap.set(
-        "",
-        "t",
-        function()
-          hop.hint_char1 {
-            direction = directions.AFTER_CURSOR,
-            current_line_only = false,
-            hint_offset = 0,
+    keys = function()
+      local function hop_like(forward, offset)
+        return function()
+          require("flash").jump {
+            search = {
+              forward = forward,
+              wrap = false,
+              multi_window = false,
+              max_length = 1,
+              mode = function(str) return "\\V" .. str end,
+            },
+            jump = {
+              offset = offset,
+            },
           }
-        end,
-        { remap = true, silent = true }
-      )
-      vim.keymap.set(
-        "",
-        "T",
-        function()
-          hop.hint_char1 {
-            direction = directions.BEFORE_CURSOR,
-            current_line_only = false,
-            hint_offset = 0,
-          }
-        end,
-        { remap = true, silent = true }
-      )
+        end
+      end
+
+      return {
+        { "<Leader>s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
+        { "f", mode = { "n", "x", "o" }, hop_like(true, 0), desc = "Hop-like f" },
+        { "F", mode = { "n", "x", "o" }, hop_like(false, 0), desc = "Hop-like F" },
+        { "t", mode = { "n", "x", "o" }, hop_like(true, -1), desc = "Hop-like t" },
+        { "T", mode = { "n", "x", "o" }, hop_like(false, 1), desc = "Hop-like T" },
+      }
     end,
   },
 }
