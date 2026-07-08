1da9817d2609ea2a577ea0d15ccdfc622d092f8c
gogongxt <gogongxt@163.com>
2026-07-08 16:47:27 +0800

    [nvim] update scratch nvim

--- nvim/.config/nvim/lua/plugins/_scratch_.lua
+++ nvim/.config/nvim/lua/plugins/_scratch_.lua
@@ -1,18 +1,19 @@
 -- if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
 
 return {
-  "LintaoAmons/scratch.nvim",
+  "gogongxt/scratch.nvim",
+  branch = "fix/picker-nav-and-delete",
   dependencies = { "nvim-lua/plenary.nvim" },
   event = "VeryLazy",
   config = function()
     require("scratch").setup {
       scratch_file_dir = vim.fn.stdpath "cache" .. "/scratch.nvim", -- where your scratch files will be put
-      window_cmd = "rightbelow vsplit", -- 'vsplit' | 'split' | 'edit' | 'tabedit' | 'rightbelow vsplit'
+      window_cmd = "edit",                                          -- 'vsplit' | 'split' | 'edit' | 'tabedit' | 'rightbelow vsplit'
       use_telescope = false,
       -- fzf-lua is recommanded, since it will order the files by modification datetime desc. (require rg)
       file_picker = "snacks",
       filetypes = { "txt", "md", "log", "yaml", "lua", "js", "sh", "ts", "cpp", "cc", "c", "py" }, -- you can simply put filetype here
-      filetype_details = { -- or, you can have more control here
+      filetype_details = {
         -- ["yaml"] = {},
         -- go = {
         --   requireDir = true, -- true if each scratch file requires a new directory
