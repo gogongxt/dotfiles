573f76c3bb7901f7dc3169c7b1ea0d98fbb5634d
gogongxt <gogongxt@163.com>
2026-07-27 20:01:11 +0800

    [nvim] update lc plugin config

--- nvim/.config/nvim/lua/plugins/_leetcode_.lua
+++ nvim/.config/nvim/lua/plugins/_leetcode_.lua
@@ -1,67 +1,87 @@
 if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
 
+-- TODO: add image support for snacks image and use ft https://github.com/kawre/leetcode.nvim/pull/158
+
 local mappings = require "mappings"
 mappings.set_mappings {
   n = {
     -- leetcode
     ["<Leader>L"] = { name = "Leetcode" },
-    ["<Leader>Ll"] = { "<cmd>Leet list<CR>", desc = "Leet list", noremap = true, silent = true },
-    ["<Leader>Lm"] = { "<cmd>Leet menu<CR>", desc = "Leet menui", noremap = true, silent = true },
-    -- [<Leader>"Lt"] = { "<cmd>Leet tabs<CR>", "desc = Leet tabs" , noremap = true, silent = true},
+    ["<Leader>LL"] = {
+      function()
+        if vim.fn.exists ":Leet" ~= 2 then
+          vim.cmd "Leet"
+          return
+        end
+        local ok, state = pcall(function() return _Lc_state.menu end)
+        if ok and state and state.winid and vim.api.nvim_win_is_valid(state.winid) then
+          vim.cmd "Leet menu"
+        else
+          vim.cmd "Leet"
+        end
+      end,
+      desc = "Leet menu",
+      noremap = true,
+      silent = true,
+    },
+    ["<leader>Ll"] = { "<cmd>Leet tabs<CR>", desc = "Leet tabs", noremap = true, silent = true },
     ["<leader>Lr"] = { "<cmd>Leet run<CR>", desc = "Leet run", noremap = true, silent = true },
-    ["<leader>Lt"] = { "<cmd>Leet test<CR>", desc = "Leet test", noremap = true, silent = true },
+    ["<leader>Lt"] = { "<cmd>Leet console<CR>", desc = "Leet console", noremap = true, silent = true },
     ["<leader>Ls"] = { "<cmd>Leet submit<CR>", desc = "Leet submit", noremap = true, silent = true },
     ["<leader>Lc"] = { "<cmd>Leet cache<CR>", desc = "Leet cache", noremap = true, silent = true },
     ["<leader>Li"] = { "<cmd>Leet info<CR>", desc = "Leet info", noremap = true, silent = true },
     ["<leader>L<tab>"] = { "<cmd>Leet desc<CR>", desc = "Leet desc", noremap = true, silent = true },
-    ["<leader>Lv"] = { "<cmd>Leet console<CR>", desc = "Leet console", noremap = true, silent = true },
   },
 }
 
 return {
-  -- 在neovim中刷leetcode
-  enabled = require("my_sys").GetConfig("config_", "leetcode.leetcode_enabled", true),
-
-  "kawre/leetcode.nvim",
+  "gogongxt/leetcode.nvim",
+  cmd = "Leet",
   build = ":TSUpdate html",
   dependencies = {
-    "nvim-telescope/telescope.nvim",
-    "nvim-lua/plenary.nvim", -- required by telescope
-    "MunifTanjim/nui.nvim",
-
-    -- optional
-    "nvim-treesitter/nvim-treesitter",
-    "rcarriga/nvim-notify",
-    "nvim-tree/nvim-web-devicons",
+    "folke/snacks.nvim",
   },
   opts = {
-    -- configuration goes here
-    --
-    ---@type lc.lang
-    -- lang = "cpp",
-    lang = require("my_sys").GetConfig("config_", "leetcode.lang", "cpp"),
+    lang = "cpp",
+
     cn = { -- leetcode.cn
-      -- enabled = true, ---@type boolean
-      -- translator = false, ---@type boolean
-      -- translate_problems = true, ---@type boolean
-      enabled = require("my_sys").GetConfig("config_", "leetcode.cn.cn_enabled", true),
-      translator = require("my_sys").GetConfig("config_", "leetcode.cn.translator", false),
-      translate_problems = require("my_sys").GetConfig("config_", "leetcode.cn.translate_problems", true),
+      enabled = true, ---@type boolean
+      translator = false, ---@type boolean
+      translate_problems = true, ---@type boolean
     },
+
+    storage = {
+      home = "~/Projects/leetcode/nvim",
+    },
+
+    plugins = {
+      non_standalone = false,
+    },
+
     injector = { ---@type table<lc.lang, lc.inject>
       ["cpp"] = {
         before = {
           "#ifdef __linux__",
+          "",
           "#include <bits/stdc++.h>",
           "",
-          '#include "/home/gxt_kt/Projects/debugstream/debugstream.hpp"',
-          '#include "/home/gxt_kt/.local/share/nvim/leetcode/nodetree.hpp"',
+          '#include "/home/gogongxt/Projects/debugstream/include/debugstream/debugstream.h"',
+          '#include "/home/gogongxt/Projects/debugstream/include/debugstream/detail/leetcode.h"',
+          '#include "/home/gogongxt/Projects/debugstream/include/debugstream/detail/leetcode_list.h"',
+          '#include "/home/gogongxt/Projects/debugstream/include/debugstream/detail/leetcode_tree.h"',
+          "",
           "#elif __APPLE__",
-          '#include "/Users/gxt_kt/Projects/debugstream/debugstream.hpp"',
-          '#include "/Users/gxt_kt/.local/share/nvim/leetcode/nodetree.hpp"',
+          "",
+          '#include "/Users/gogongxt/Projects/debugstream/include/debugstream/debugstream.h"',
+          '#include "/Users/gogongxt/Projects/debugstream/include/debugstream/detail/leetcode.h"',
+          '#include "/Users/gogongxt/Projects/debugstream/include/debugstream/detail/leetcode_list.h"',
+          '#include "/Users/gogongxt/Projects/debugstream/include/debugstream/detail/leetcode_tree.h"',
           '#include "stdc++.h"',
+          "",
           "#elif _WIN32",
+          "",
           "#endif",
+          "",
           "using namespace std;",
         },
         after = {
@@ -72,25 +92,18 @@ return {
           "}",
         },
       },
-      ["java"] = {
-        before = "import java.util.*;",
-      },
     },
     keys = {
       toggle = { "q", "<Esc>" }, ---@type string|string[]
       confirm = { "<CR>" }, ---@type string|string[]
-
       reset_testcases = "r", ---@type string
-      use_testcase = "U", ---@type string
+      use_testcase = "a", ---@type string
       focus_testcases = "<C-h>", ---@type string
       focus_result = "<C-l>", ---@type string
     },
 
-    ---@type boolean
-    image_support = (function()
-      local leetcode_show_image = require("my_sys").GetConfig("config_", "leetcode.show_image", false)
-      local image_enabled = require("my_sys").GetConfig("config_", "image.image_enabled", false)
-      return image_enabled and leetcode_show_image
-    end)(), -- setting this to `true` will disable question description wrap
+    picker = { provider = "snacks-picker" },
+
+    image_support = false,
   },
 }
