cbec19555868b3f0d085672d4630d55204a3e4af
gogongxt <gogongxt@163.com>
2026-09-09 14:59:58 +0800

    [yazi] update from 26.5.6 to 26.9.1

--- yazi/.config/yazi/plugins/eza-preview.yazi/README.md
+++ yazi/.config/yazi/plugins/eza-preview.yazi/README.md
@@ -10,13 +10,13 @@ Tree mode:
 
 ## Requirements
 
-- [yazi (25.12+) or nightly](h;nottps://github.com/sxyazi/yazi)
+- [yazi (26.1.4+) or nightly](https://github.com/sxyazi/yazi)
 - [eza (0.20+)](https://github.com/eza-community/eza)
 
 ## Installation
 
 ```sh
-ya pack -a ahkohd/eza-preview
+ya pkg add ahkohd/eza-preview
 ```
 
 ## Usage
--- yazi/.config/yazi/plugins/visual-mode.yazi/main.lua
+++ yazi/.config/yazi/plugins/visual-mode.yazi/main.lua
@@ -5,14 +5,14 @@
 
 return {
   entry = function()
-    -- 检查当前是否处于视觉模式
-    if cx.active.mode.is_visual then
-      -- 如果是，则执行 "escape" 命令退出视觉模式
+    -- 检查当前是否处于普通模式（is_visual 已在 v26.8.15 废弃，改用 is_normal）
+    if cx.active.mode.is_normal then
+      -- 普通模式 -> 执行 "visual_mode" 命令进入选择模式
+      ya.emit("visual_mode", {})
+    else
+      -- 视觉模式 -> 执行 "escape" 命令退出视觉模式
       -- 使用 { visual = true } 确保只退出视觉模式，而不影响其他状态
       ya.emit("escape", { visual = true })
-    else
-      -- 如果不是，则执行 "visual_mode" 命令进入选择模式
-      ya.emit("visual_mode", {})
     end
   end,
 }
